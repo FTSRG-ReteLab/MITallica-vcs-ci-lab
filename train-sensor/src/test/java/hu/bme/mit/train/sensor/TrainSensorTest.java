ec556d509533e7b6bc57d9cd48b35ed7502c7ca4
MITalica <meres@labor.hu>
2019-02-18 07:28:02 -0800

    Created new test for Sensor

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,36 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.system.TrainSystem;
+
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
+	TrainController controller;
+	TrainSensor sensor;
+	TrainUser user;
 
     @Before
     public void before() {
-        // TODO Add initializations
+	TrainSystem system = new TrainSystem();
+	controller = system.getController();
+	sensor = system.getSensor();
+	user = system.getUser();
+
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void overrideSpeedLimitTest() {
+		int origSpeed = sensor.getSpeedLimit();
+		sensor.overrideSpeedLimit(10);
+        //Assert.assertEquals(origSpeed, 10, "Speed should be 10 after override");
+		Assert.assertEquals(origSpeed, 10);
+
     }
 }
