65397100b1eabf09d435193883d5a911e64d6d54
MITalica <meres@labor.hu>
2019-02-18 07:51:27 -0800

    and this

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,7 +3,7 @@ package hu.bme.mit.train.sensor;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
-import hu.bme.mit.train.system.TrainSystem;
+
 
 
 import org.junit.Assert;
@@ -12,25 +12,15 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
-	TrainController controller;
-	TrainSensor sensor;
-	TrainUser user;
+
 
     @Before
     public void before() {
-	TrainSystem system = new TrainSystem();
-	controller = system.getController();
-	sensor = system.getSensor();
-	user = system.getUser();
-
+		// TODO
     }
 
     @Test
     public void overrideSpeedLimitTest() {
-		int origSpeed = sensor.getSpeedLimit();
-		sensor.overrideSpeedLimit(10);
-        //Assert.assertEquals(origSpeed, 10, "Speed should be 10 after override");
-		Assert.assertEquals(origSpeed, 10);
-
+		// TODO
     }
 }
