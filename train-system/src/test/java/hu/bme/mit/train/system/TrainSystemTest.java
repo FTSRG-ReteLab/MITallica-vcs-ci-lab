239b785de4b02a8add66f4cc3f62b10d11d444ee
MITalica <meres@labor.hu>
2019-02-18 07:43:19 -0800

    New test for train-system

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,12 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
+	@Test
+	public void speedOverrideTest() {
+		int origSpeed = sensor.getSpeedLimit();
+		sensor.overrideSpeedLimit(50);
+		Assert.assertEquals(origSpeed, 50);
+	}
+
 	
 }
